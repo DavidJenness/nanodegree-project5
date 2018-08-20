0187d4aa8e555d9685febb534f54b1b5faf0bddb
David Jenness <David4FSU@gmail.com>
2018-08-20 05:34:00 -0500

    Cleaned up Service Worker Code

--- sw.js
+++ sw.js
@@ -1,49 +1,11 @@
 var CACHE_NAME = 'my-site-cache-v1';
-// var urlsToCache = [
-//   '/',
-//   '/css/styles.css',
-//   '/data/restaurants.json',
-//   '/img/1.jpg',
-//   '/img/2.jpg',
-//   '/img/3.jpg',
-//   '/img/4.jpg',
-//   '/img/5.jpg',
-//   '/img/6.jpg',
-//   '/img/7.jpg',
-//   '/img/8.jpg',
-//   '/img/9.jpg',
-//   '/img/10.jpg',
-//   '/index.html',
-//   '/js/apikey.js',
-//   '/js/dbhelper.js',
-//   '/js/main.js',
-//   '/js/restaurant_info.js',
-//   '/sw.js'
-// ];
-
-// console.log('Running sw.js Dave!');
-
-//Need to Cache the results of the street view as well. Problem is getting variable from the apikey.js into the service worker
-// var myKey = 'https://api.tiles.mapbox.com/v4/mapbox.streets.html?access_token=' + self.MapAPIkey;
-// console.log('My key is ' + myKey + ',Dave!');
-
 
  self.addEventListener('install', function(event) {
   console.log('Performed Install, Dave!');
   event.waitUntil(
-    caches.open('my-site-cache-v1').then(cache => cache.addAll(['/index.html',
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['/index.html',
     '/css/styles.css',
     '/data/restaurants.json',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg',
     '/js/apikey.js',
     '/js/dbhelper.js',
     '/js/main.js',
@@ -57,7 +19,7 @@ var CACHE_NAME = 'my-site-cache-v1';
 
   self.addEventListener('activate', function(event) {
 
-    var cacheWhitelist = ['my-site-cache-v1'];
+    var cacheWhitelist = [CACHE_NAME];
     console.log('Performed Activate, Dave!');
     self.skipWaiting();
     event.waitUntil(
@@ -73,25 +35,6 @@ var CACHE_NAME = 'my-site-cache-v1';
     );
   });
 
-
-
-
-  // self.addEventListener('fetch', function(event) {
-  //   console.log('Performed Fetch, Dave!' + event.request.url);
-  //   event.respondWith(
-  //     caches.match(event.request)
-  //       .then(function(response) {
-  //         // Cache hit - return response
-  //         if (response) {
-  //           console.log('Fetch Success, Dave! '  + event.request.url);
-  //           return response;
-  //         }
-  //         console.log('Fetch Failure, Dave! ' + event.request.url);
-  //         return fetch(event.request);
-  //       }
-  //     )
-  //   );
-  // });
 
   self.addEventListener('fetch', function(event) {
     event.respondWith(
