0c05830af28466edd401703e7493ae2f88191519
David Jenness <David4FSU@gmail.com>
2018-08-22 06:06:54 -0500

    improving sw offline caching

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ self.addEventListener('install', function (event) {
   console.log('Performed Install');
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(['/index.html',
+      'restaurant.html',
       '/css/styles.css',
       '/data/restaurants.json',
       '/js/apikey.js',
@@ -11,7 +12,17 @@ self.addEventListener('install', function (event) {
       '/js/main.js',
       '/js/restaurant_info.js',
       '/sw.js',
-      '/'
+      '/',
+      '/img/1.jpg',
+      '/img/2.jpg',
+      '/img/3.jpg',
+      '/img/4.jpg',
+      '/img/5.jpg',
+      '/img/6.jpg',
+      '/img/7.jpg',
+      '/img/8.jpg',
+      '/img/9.jpg',
+      '/img/10.jpg'
     ]))
   );
 });
@@ -39,7 +50,7 @@ self.addEventListener('activate', function (event) {
 self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.open(CACHE_NAME).then(function (cache) {
-      return cache.match(event.request).then(function (response) {
+      return cache.match(event.request,{ignoreSearch:true}).then(function (response) {
         return response || fetch(event.request).then(function (response) {
           cache.put(event.request, response.clone());
           return response;
